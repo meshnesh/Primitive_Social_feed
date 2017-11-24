a19e5c1fe8b3405bac3739cb1b6495f1b1ad7a44
meshnesh <tonnie.nesh@gmail.com>
2017-11-24 13:07:21 +0300

    modify user prompt

--- models/primitive.py
+++ models/primitive.py
@@ -1,19 +1,11 @@
 from urllib import request, parse
 import requests
 
-r = requests.get('https://jsonplaceholder.typicode.com/posts')
-json_list = r.json()
-# for post in json_list:
-    # print (post["title"])
-
-
-
 def get_post():
     r = requests.get('https://jsonplaceholder.typicode.com/posts')
     json_list = r.json()
     for post in json_list:
         print (post["title"])
-
 
 def create_post():
     r = requests.post('https://jsonplaceholder.typicode.com/posts')
@@ -24,16 +16,17 @@ def create_post():
     request_ = requests.Request(r, data=data)
     return request_
 
-print("Select operation.")
-print("1.View Posts")
-print("2.Create Posts")
+print ("Select operation.")
+print ("1.View Posts")
+print ("2.Create Post")
+print ("3.Comment to a post")
 
-choice = int(input("Enter choice(1/2): "))
+choice = int(input("Enter choice(1/2/3): "))
 if choice == 1:
     get_post()
 elif choice == 2:
     print (create_post())
+elif choice == 3:
+    print("Coming soon!!")
 else:
     print("Invalid choice")
-
-
