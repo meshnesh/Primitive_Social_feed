20ecba7aeccbdbc60ace321b758974a39f233c01
meshnesh <tonnie.nesh@gmail.com>
2017-11-24 12:33:33 +0300

    implement create new post function

--- models/primitive.py
+++ models/primitive.py
@@ -3,8 +3,8 @@ import requests
 
 r = requests.get('https://jsonplaceholder.typicode.com/posts')
 json_list = r.json()
-for post in json_list:
-    print (post["title"])
+# for post in json_list:
+    # print (post["title"])
 
 
 
@@ -13,3 +13,16 @@ for post in json_list:
 #      return post["title"]
 # print (get_post(json_list))
 
+def create_post():
+    r = requests.post('https://jsonplaceholder.typicode.com/posts')
+    title = input("Enter Post Title\n:")
+    body = input("Write Post Body\n:")
+    req = {'tittle':title, 'body':body}
+    data = parse.urlencode(req).encode()
+    request_ = requests.Request(r, data=data)
+    return request_
+    # resp = request.urlopen(request_)
+    # return resp
+print (create_post())
+
+# print (json_list)
